b8fa4df9b6215379ab14b7ebed39bccb7e933c08
Nevold <Nevold@tut.by>
2025-03-04 23:00:40 +0300

    add

--- decision-making-tool/.eslintrc.js
+++ decision-making-tool/.eslintrc.js
@@ -8,7 +8,7 @@ module.exports = {
     'airbnb-base',
     'airbnb-typescript/base',
     'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-type-checked',
     'prettier'
   ],
   parser: '@typescript-eslint/parser',
@@ -32,6 +32,17 @@ module.exports = {
     'no-console': 'warn',
     'max-len': ['warn', { code: 120 }],
     'import/prefer-default-export': 'off',
-    '@typescript-eslint/lines-between-class-members': 'off'
-  }
+    '@typescript-eslint/lines-between-class-members': 'warn',
+    '@typescript-eslint/consistent-type-assertions': ['error', { assertionStyle: 'never' }],
+    '@typescript-eslint/consistent-type-imports': 'error',
+    '@typescript-eslint/explicit-function-return-type': 'error',
+    '@typescript-eslint/explicit-member-accessibility': [
+      'error',
+      { accessibility: 'explicit', overrides: { constructors: 'off' } }
+    ],
+    '@typescript-eslint/member-ordering': 'error',
+    'class-methods-use-this': 'error'
+  },
+  noInlineConfig: true,
+  reportUnusedDisableDirectives: true
 };
