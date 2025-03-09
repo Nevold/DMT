3b7e5df67cde423bf6f3fc4e1f8ee5cd0c6be961
Nevold <Nevold@tut.by>
2025-03-09 18:50:59 +0300

    add

---
+++ decision-making-tool/src/components/tags.ts
@@ -0,0 +1,7 @@
+import { BaseComponent } from './base-component';
+
+export const main = new BaseComponent('main', 'div');
+
+export const h1 = new BaseComponent('heading', 'h1', 'Decision Making Tool');
+
+export const list = new BaseComponent('list', 'ul');
--- decision-making-tool/src/index.ts
+++ decision-making-tool/src/index.ts
@@ -1,14 +1,15 @@
 import { BaseComponent } from './components/base-component';
+import { main, h1, list } from './components/tags';
 import './global.css';
 
 // const app = new App();
 // app.start();
 
-const main = new BaseComponent('main', 'div').getNode();
-const heading = new BaseComponent('heading', 'h1', 'Decision Making Tool').getNode();
-const list = new BaseComponent('list', 'ul').getNode();
+const mainNode = main.getNode();
+const h1Node = h1.getNode();
+const listNode = list.getNode();
 const option = new BaseComponent('option', 'li').getNode();
 
-document.body.append(main);
-main.append(heading, list);
-list.append(option);
+document.body.append(mainNode);
+mainNode.append(h1Node, listNode);
+listNode.append(option);
