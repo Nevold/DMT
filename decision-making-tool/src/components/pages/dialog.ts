93d98a4ef4dc99c21e74b6c90684ba7423f3022a
Nevold <Nevold@tut.by>
2025-03-13 21:55:59 +0300

    add

--- decision-making-tool/src/components/pages/dialog.ts
+++ decision-making-tool/src/components/pages/dialog.ts
@@ -1,5 +1,8 @@
+import { StorageService } from '../../services/local-storage.service';
+import { Utils } from '../../shared/utils/utils';
 import type { NodeType } from '../../types/types';
 import { BaseComponent } from '../base-component';
+import Tags from '../tags';
 
 export class Dialog {
   public static readonly dialogNode = new BaseComponent('dialog', 'dialog').getNode();
@@ -44,9 +47,31 @@ export class Dialog {
           })
           .map(element => element.map(item => item?.trim()))
           .filter(element => Number(element[1]));
+        StorageService.getData();
+        const storageOptionsList = optionsList.map((item, index) => {
+          if (item[0] && item[1]) {
+            return {
+              id: `#${StorageService.data.lastId + index + 1}`,
+              title: item[0],
+              weight: item[1]
+            };
+          }
+          return item;
+        });
+        const storageData = {
+          list: [...StorageService.data.list, ...storageOptionsList],
+          lastId: StorageService.data.lastId + storageOptionsList.length
+        };
+        if (StorageService.isDatabase(storageData)) {
+          StorageService.data = Utils.sortById(storageData);
+          StorageService.saveData(Utils.sortById(storageData));
+        }
 
-        const storageOptionsList = optionsList.map(item => ({ id: '31', title: item[0], weight: item[1] }));
-        console.log(storageOptionsList); // доделать
+        Tags.listNode.replaceChildren();
+        Tags.childrenList = StorageService.data.list.map(node => Tags.li(node.id, node.title, node.weight));
+        Tags.listNode.append(...Tags.childrenList);
+
+        this.formNode.elements[0].value = '';
       }
     });
 
@@ -74,7 +99,7 @@ export class Dialog {
       if (event.target && event.target instanceof HTMLButtonElement && this.dialogNode instanceof HTMLDialogElement) {
         event.stopPropagation();
         event.preventDefault();
-        // this.dialogNode.close();
+        this.dialogNode.close();
       }
     });
   };
