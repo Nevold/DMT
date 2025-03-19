5278ce75d0bb60988798a712b4c0c645360fe6d8
Nevold <nevold@tut.by>
2025-03-19 14:17:16 +0300

    fix

--- decision-making-tool/src/index.ts
+++ decision-making-tool/src/index.ts
@@ -1,9 +1,11 @@
 import { Options } from './components/controls/options';
+import { OptionsModal } from './components/controls/options-modal';
 import { Nodes } from './components/nodes';
 import { DecisionPicker } from './components/pages/decision-picker';
 import { ListOfOptions } from './components/pages/list-of-options';
 import { NotFound } from './components/pages/not-found';
 import './global.css';
+import { StorageService } from './services/local-storage.service';
 import { HashRouter } from './services/router.service';
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -22,10 +24,17 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 
   HashRouter.addRoute('/decision-picker', () => {
-    Nodes.labelDurationNode.replaceChildren();
-    Nodes.formPickNode.replaceChildren();
-    Nodes.main.replaceChildren();
-    DecisionPicker.start();
+    StorageService.getData();
+    const isRule = StorageService.data.list.filter(element => element.title !== '' && +element.weight > 0).length >= 2;
+
+    if (isRule) {
+      Nodes.labelDurationNode.replaceChildren();
+      Nodes.formPickNode.replaceChildren();
+      Nodes.main.replaceChildren();
+      DecisionPicker.start();
+    } else {
+      HashRouter.navigateTo('/');
+    }
   });
 
   HashRouter.setNotFound(() => {
